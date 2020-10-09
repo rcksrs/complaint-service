3bb4f85727469904810b1f45b1a6eacb1109cd9d
Eryck Soares <rcksrs@outlook.com>
2020-10-09 09:29:35 -0300

    Initial commit

---
+++ src/test/java/com/rcksrs/complaintservice/ComplaintServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcksrs.complaintservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
