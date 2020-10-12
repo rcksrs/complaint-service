eb226fa99f15402f9846b986a590ad169a278a86
Eryck Soares <rcksrs@outlook.com>
2020-10-12 12:01:55 -0300

    Added endpoints documentation with swagger

--- src/main/java/com/rcksrs/complaintservice/controller/ComplaintController.java
+++ src/main/java/com/rcksrs/complaintservice/controller/ComplaintController.java
@@ -22,6 +22,8 @@ import com.rcksrs.complaintservice.domain.Complaint;
 import com.rcksrs.complaintservice.domain.Reply;
 import com.rcksrs.complaintservice.service.ComplaintService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/v1/complaint")
 public class ComplaintController {
@@ -30,48 +32,56 @@ public class ComplaintController {
 	private ComplaintService complaintService;
 	
 	@GetMapping("/filter/title/{title}")
+	@ApiOperation("Return all complaints that are registered filtered by title")
 	public ResponseEntity<Page<Complaint>> findAllByTitle(@PathVariable String title, @PageableDefault(sort = "name", size = 20) Pageable pageable) {
 		var complaints = complaintService.findAllByTitle(title, pageable);
 		return ResponseEntity.ok(complaints); 
 	}
 	
 	@GetMapping("/filter/description/{description}")
+	@ApiOperation("Return all complaints that are registered filtered by description")
 	public ResponseEntity<Page<Complaint>> findAllByDescription(@PathVariable String description, @PageableDefault(sort = "name", size = 20) Pageable pageable) {
 		var complaints = complaintService.findAllByDescription(description, pageable);
 		return ResponseEntity.ok(complaints);
 	}
 	
 	@GetMapping("/filter/user/{userId}")
+	@ApiOperation("Return all complaints that are registered filtered by user")
 	public ResponseEntity<Page<Complaint>> findAllByUserId(@PathVariable String userId, @PageableDefault(sort = "name", size = 20) Pageable pageable) {
 		var complaints = complaintService.findAllByUserId(userId, pageable);
 		return ResponseEntity.ok(complaints);
 	}
 	
 	@GetMapping("/filter/user/locale")
+	@ApiOperation("Return all complaints that are registered filtered by state and city of the user")
 	public ResponseEntity<Page<Complaint>> findAllByUserStateAndUserCity(@RequestParam String state, @RequestParam String city, @PageableDefault(sort = "name", size = 20) Pageable pageable) {
 		var complaints = complaintService.findAllByUserStateAndUserCity(state, city, pageable);
 		return ResponseEntity.ok(complaints);
 	}
 	
 	@GetMapping("/filter/company/{companyId}")
+	@ApiOperation("Return all complaints that are registered filtered by company")
 	public ResponseEntity<Page<Complaint>> findAllByCompanyId(@PathVariable String companyId, @PageableDefault(sort = "name", size = 20) Pageable pageable) {
 		var complaints = complaintService.findAllByCompanyId(companyId, pageable);
 		return ResponseEntity.ok(complaints);
 	}
 	
 	@GetMapping("/filter/company/locale")
+	@ApiOperation("Return all complaints that are registered filtered by state and city of the company")
 	public ResponseEntity<Page<Complaint>> findAllByCompanyStateAndCity(@RequestParam String state, @RequestParam String city, @PageableDefault(sort = "name", size = 20) Pageable pageable) {
 		var complaints = complaintService.findAllByCompanyStateAndCity(state, city, pageable);
 		return ResponseEntity.ok(complaints);
 	}
 	
 	@GetMapping("/{id}")
+	@ApiOperation("Find a complaint by id")
 	public ResponseEntity<Complaint> findById(@PathVariable String id) {
 		var complaint = complaintService.findById(id);
 		return ResponseEntity.ok(complaint);
 	}
 	
 	@PostMapping
+	@ApiOperation("Save a complaint")
 	public ResponseEntity<Complaint> save(@RequestBody @Valid Complaint complaint) {
 		var complaintSaved = complaintService.save(complaint);
 		var location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(complaintSaved.getId()).toUri();
@@ -79,21 +89,23 @@ public class ComplaintController {
 	}
 	
 	@PostMapping("/reply")
+	@ApiOperation("Reply a complaint")
 	public ResponseEntity<Complaint> reply(@RequestBody @Valid Reply reply) {
 		var complaint = complaintService.reply(reply);
 		return ResponseEntity.ok(complaint);
 	}
 	
 	@PostMapping("/close")
+	@ApiOperation("Close a complaint")
 	public ResponseEntity<Complaint> close(@RequestBody @Valid Complaint complaint) {
 		var complaintSaved = complaintService.close(complaint);
 		return ResponseEntity.ok(complaintSaved);
 	}
 	
 	@DeleteMapping
+	@ApiOperation("Delete a complaint")
 	public ResponseEntity<Complaint> delete(@RequestBody @Valid Complaint complaint) {
 		complaintService.delete(complaint);
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
-
 }
--- src/main/java/com/rcksrs/complaintservice/controller/UserController.java
+++ src/main/java/com/rcksrs/complaintservice/controller/UserController.java
@@ -21,6 +21,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.rcksrs.complaintservice.domain.User;
 import com.rcksrs.complaintservice.service.UserService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
@@ -29,30 +31,35 @@ public class UserController {
 	private UserService userService;
 	
 	@GetMapping("/cpf/{cpf}")
+	@ApiOperation("Find a user by CPF")
 	public ResponseEntity<User> findByCpf(@PathVariable String cpf) {
 		var user = userService.findByCpf(cpf);
 		return ResponseEntity.ok(user);
 	}
 	
 	@GetMapping("/{id}")
+	@ApiOperation("Find a user by id")
 	public ResponseEntity<User> findById(@PathVariable String id) {
 		var user = userService.findById(id);
 		return ResponseEntity.ok(user);
 	}
 	
 	@GetMapping("/filter/name/{name}")
+	@ApiOperation("Find a user by name")
 	public ResponseEntity<Page<User>> findAllByName(@PathVariable String name, @PageableDefault(sort = "name", size = 20) Pageable pageable) {
 		var users = userService.findAllByName(name, pageable);
 		return ResponseEntity.ok(users);
 	}
 	
 	@GetMapping("/name/{name}")
+	@ApiOperation("Find a user by exact name")
 	public ResponseEntity<Page<User>> findAllByExactName(@PathVariable String name, @PageableDefault(sort = "name", size = 20) Pageable pageable) {
 		var users = userService.findAllByExactName(name, pageable);
 		return ResponseEntity.ok(users);
 	}
 	
 	@PostMapping
+	@ApiOperation("Save a user")
 	public ResponseEntity<User> save(@RequestBody @Valid User user) {
 		var userSaved = userService.save(user);
 		var location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(userSaved.getId()).toUri();
@@ -60,12 +67,14 @@ public class UserController {
 	}
 	
 	@PutMapping
+	@ApiOperation("Update a user")
 	public ResponseEntity<User> update(@RequestBody @Valid User user) {
 		var userSaved = userService.update(user);
 		return ResponseEntity.ok(userSaved);
 	}
 	
 	@DeleteMapping
+	@ApiOperation("Delete a user")
 	public ResponseEntity<User> delete(@RequestBody @Valid User user) {
 		userService.delete(user);
 		return ResponseEntity.status(HttpStatus.OK).build();
